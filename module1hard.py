56377d703b990ed5f26b06a30a977be3dc39f0fe
Elena-1717 <kea5599@mail.ru>
2024-07-09 16:54:01 +0300

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -1,26 +1,13 @@
-#grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-a = [5, 3, 3, 5, 4]
-average = sum(a) / len(a)
-print(average)
-b = [2, 2, 2, 3]
-average = sum(b) / len(b)
-print(average)
-c = [4, 5, 5, 2]
-average = sum(c) / len(c)
-print(average)
-d = [4, 4, 3]
-average = sum(d) / len(d)
-print(average)
-e = [5, 5, 5, 4, 5]
-average = sum(e) / len(e)
-print(average)
-grades = {'a', 'b', 'c', 'd', 'e'}
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-print(sorted(grades))
-print(sorted(students))
-slovar = {'Aaron': 4.0, 'Bilbo':2.25, 'Johnny': 4.0, 'Khendrik': 3.6666666666666665, 'Steve': 4.8}
-print(slovar)
 
+students_list = sorted(list(students))
+
+grades_average_list = [(sum(grades[0]) / len(grades[0])),(sum(grades[1]) / len(grades[1])),(sum(grades[2]) / len(
+grades[2])),(sum(grades[3]) / len(grades[3])),(sum(grades[4]) / len(grades[4]))]
+
+average_grades_dict = dict(zip(students_list, grades_average_list))
+print(average_grades_dict)
 
 
 
